9b9cdaae45c8d7a5264d536d41c22cf397f634ee
Samarth Dev <89363509+sdev138@users.noreply.github.com>
2021-12-02 20:42:27 -0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App extends TestingApp {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+class TestingApp { 
+    {
+
+    for (int i = 0; i < 10; i++) {
+        System.out.println("Hello World"); 
+    }
+}
+}
+
+
